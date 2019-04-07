b58eaba5ed7254a6b2c6f553c1595f8cab8c06c6
原田浩輝 <haradahiroki@haradahirokinoMacBook-Pro.local>
2019-04-07 18:08:06 +0900

    [Start]2-8

---
+++ for.rb
@@ -0,0 +1,29 @@
+# getsとto_i
+puts "数字を入力してください"
+	a=gets.to_i
+	b=gets.to_i
+puts "a+b=#{a+b}"
+# while文
+dice = 0
+while dice != 6 do
+	dice = rand(1..6)
+	puts dice
+end
+# for文
+for i in 1..10 do
+	puts i
+end
+# eachメソッド
+{"apple" => 130, "strawberry" => 180, "orange" => 100}.each do | fruit, price|
+	puts "#{fruit}は#{price}円です。"
+end
+# break
+i = 0
+while i <= 10 do
+	if i > 5
+		break
+	end
+	puts i
+	i+= 1
+end
+
