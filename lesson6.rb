13360a56846abc99a18c3f1a48d38134f13e570c
原田浩輝 <haradahiroki@haradahirokinoMacBook-Pro.local>
2019-04-07 16:32:54 +0900

    [Start]2-7

---
+++ lesson6.rb
@@ -0,0 +1,34 @@
+apple = 110
+# true
+if apple >= 100
+	puts "りんごの値段は100円以上です"
+end
+# false
+if apple >= 100
+	puts "りんごの値段は100円以上です"
+end
+
+
+tall = 180
+
+if tall >= 170 && tall <= 190
+	puts "身長は170以上190以下です"
+end
+
+apple = "Aomori"
+
+if apple == "Nagano"
+	puts "りんごの生産地は長野です"
+else
+	puts "りんごの生産地は青森です"
+end
+
+apple ="Yamagata"
+
+if apple == "Nagano"
+	puts "りんごの生産地は長野です"
+elsif apple =="Yamagata"
+	puts "りんごの生産地は山形です"
+else
+	puts "りんごの生産地は青森です"
+end
